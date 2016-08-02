cb2c91283a1e8a812fc4f3156cfe5af90da8ec41
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-08-02 03:06:57 +0000

    added static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#top'
+  get 'home', to: 'static_pages#home'
   resources :sessions, only: [:create, :destroy, :new]
   get 'login',  to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
