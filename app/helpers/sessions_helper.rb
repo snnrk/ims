3355390cdfd7a2385e07406cd132a4930a47e9eb
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-08-05 01:21:39 +0000

    added login process

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def current_user?(user)
+    @current_user == user
+  end
+  
+  def logged_in?
+    current_user.present?
+  end
 end
