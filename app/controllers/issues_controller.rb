1c941882a8c42a9b818dd4558963b4494ff107ab
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-08-09 01:49:52 +0000

    added issues_controller#index

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,6 @@
 class IssuesController < ApplicationController
+  def index
+    @user = User.find(params[:user_id]) || current_user
+    @issues = @user.written_issues
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   # static_pages
   root to: 'static_pages#top'
   get 'home', to: 'static_pages#home'
-  
-  # users
-  resources :users, only: [:create, :new, :show]
-  get 'signup', to: 'users#new'
+
+  # issues
+  resources :issues, only: [:index, :show]
   
   # sessions
   resources :sessions, only: [:create, :destroy, :new]
   get 'login',  to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # users
+  resources :users, only: [:create, :new, :show]
+  get 'signup', to: 'users#new'
 end
